37b3cab5e845092c74078ff2a8c89641f1543169
Loor Jhon <jloor2378@utm.edu.ec>
2023-02-17 22:20:10 -0500

    Delete comidas directory

--- comidas/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from.models import Banquete
-
-# Register your models here.
-class ProjectAdmin(admin.ModelAdmin):
-    readonly_fields = ('create', 'updated' )
-
-
-admin.site.register(Banquete, ProjectAdmin)
--- comidas/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComidasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'comidas'
-    verbose_name = "Banquetes"
